d42f92c88256941f57f6b3c141d5d2d5586ecbda
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-04-26 15:29:55 -0300

    list created in js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import _ from 'lodash';
+import './style.css';
+const list = document.getElementById('list');
 
 let tasks = [
   {
@@ -17,3 +19,18 @@ let tasks = [
     index: '3',
   },
 ]
+
+function createToDoList() {
+  const title = document.createElement('h1');
+  title.innerHTML= `To do List`;
+  const ulList = document.createElement('ul');
+  list.appendChild(title);
+  for (let i = 0; i < tasks.length; i++) {
+    const liTask =document.createElement('li');
+    liTask.innerHTML =`${tasks[i].description}`;
+    ulList.appendChild(liTask);
+  }
+  list.appendChild(ulList);
+}
+
+window.addEventListener('load', createToDoList());
