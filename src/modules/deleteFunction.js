7964b5ddf5a120f7412bdec8357cbe6b04a7008e
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-04-28 13:42:37 -0300

    delete console log

--- src/modules/deleteFunction.js
+++ src/modules/deleteFunction.js
@@ -1,6 +1,5 @@
 export default function deleteTask(liElement, array) {
   const number = liElement.dataset.index;
-  console.log(number)
   array.splice(number - 1, 1);
   for (let i = number - 1; i < array.length; i += 1){
     array[i].index --;
@@ -12,6 +11,4 @@ export default function deleteTask(liElement, array) {
       element.dataset.index --;
     }
 })
-
-  console.log(array);
 }
